55ab48ef4d456658796eb53e62d00f2072e5acee
Vincent Povirk <madewokherd@gmail.com>
2009-08-30 21:13:26 -0500

    add pwd and environment tracking to the shell

--- brash.py
+++ brash.py
@@ -71,6 +71,8 @@ if _windows:
                 si = process.STARTUPINFO()
                 pi = process.PROCESS_INFORMATION()
                 try:
+                    #FIXME: use PATH and environment from the shell
+                    #FIXME: use working dir from the shell
                     process.CreateProcess(
                         None, #application name
                         cmdline, #command line
@@ -115,6 +117,36 @@ if _windows:
                 self._result = process.GetExitCodeProcess(self._hprocess)
                 ctypes.windll.kernel32.CloseHandle(self._hprocess)
                 return self._result
+
+    import UserDict
+    # the environment needs to be case-insensitive on Windows, case-sensitive on other platforms
+    class EnvironDictionary(UserDict.DictMixin):
+        def __init__(self, *args, **kwargs):
+            if args:
+                try:
+                    for kv in args[0].iteritems():
+                        self.values[kv[0].lower] = kv
+                except AttributeError:
+                    for kv in args[0]:
+                        self.values[kv[0].lower] = kv
+            for kv in kwargs.iteritems():
+                self.values[kv[0].lower] = kv
+        def copy(self):
+            return EnvironDictionary(self)
+        def __getitem__(self, key):
+            return self.values[key.lower()][1]
+        def __setitem__(self, key, value):
+            self.values[key.lower()] = (key, value)
+        def __delitem__(self, key):
+            del self.values[key.lower()]
+        def keys(self):
+            return [x[0] for x in self.values.itervalues()]
+        def __contains__(self, key):
+            return key.lower() in self.values
+        def __iter__(self):
+            return (x[0] for x in self.values.itervalues())
+        def iteritems(self):
+            return self.values.itervalues()
 else:
     class ArgBlock(CommandBlock):
         __slots__ = ['_pid', '_result', '_args']
@@ -148,8 +180,22 @@ else:
                     #FIXME: use working dir from the shell
                     #FIXME: use stdin, stdout, and stderr
                     #FIXME: close any other open fd's
+                    os.chdir(shell.curdir)
                     try:
-                        os.execv(args[0], args)
+                        if os.sep in args[0] or (os.altsep and os.altsep in args[0]):
+                            # path specifies a directory
+                            os.execve(args[0], args, shell.environ)
+                        else:
+                            # we need to search PATH
+                            for path in shell.environ.get('PATH', os.defpath).split(os.pathsep):
+                                try:
+                                    os.execve(os.path.join(path, args[0]), args, shell.environ)
+                                except OSError, e:
+                                    if e.errno != 2: #2 is no such file or directory
+                                        raise
+                            else:
+                                print("%s: %s" % (args[0], e.strerror))
+                                os._exit(e.errno)
                     except OSError, e:
                         print("%s: %s" % (args[0], e.strerror))
                         os._exit(e.errno)
@@ -183,7 +229,17 @@ else:
                 self._result = exitcode
                 return exitcode
 
+    EnvironDictionary = dict
+
 class Shell(object):
+    def __init__(self, curdir=None, environ=None):
+        if curdir is None:
+            curdir = os.getcwd()
+        if environ is None:
+            environ = EnvironDictionary(os.environ)
+        self.curdir = curdir
+        self.environ = environ
+
     def read_input(self):
         return None
 
@@ -206,12 +262,23 @@ class InteractiveShell(Shell):
         except EOFError:
             return None
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
+        Shell.__init__(self, *args, **kwargs)
         self.stdin = sys.stdin
         self.stdout = sys.stdout
         self.stderr = sys.stderr
 
+def main():
+    curdir = os.getcwd()
+    environ = EnvironDictionary(os.environ)
+    if not _windows:
+        #Since a shell's working directory isn't the process's working directory,
+        # we shouldn't keep it open as the current directory
+        os.chdir('/')
+    shell = InteractiveShell(curdir, environ)
+    shell.run()
+    print ''
+
 if __name__ == '__main__':
-    InteractiveShell().run()
-    print('')
+    main()
 
